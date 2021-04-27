773e3b24853538d2bc2466cc8f66d4bc1e56257f
nemostatus <mattcorralejo1@gmail.com>
2021-04-27 07:20:20 -0700

    created a div creator function

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function outer(){
     count = 0
-function countTo10(){
+function countTo(){
     while(count<100){
     document.getElementById("countDisplay").innerHTML += `${count},`
     count++
-    countTo10()
+    countTo() //the function is calling itself which checks condition, if it's true then increase count and recal itself until the condition is met
     }
 }
-countTo10()
+countTo() //the outer function invokes the inner function so onclick the global exe context is the outer function then inner(lifo executed)
 }
+//using a closure is good practice since having variables in global scope can be dangerous
+function create(event){
+    event.preventDefault()
+    count =0
+function inner(){
+    
+    let num = document.getElementById("num").value
+    while(count <= num){
+    document.getElementById("divDisplay").innerHTML += `<div> Div number ${count}.</div>`
+    count++
+    inner()
+}
+}
+inner()}
