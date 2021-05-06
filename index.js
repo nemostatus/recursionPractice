810ed05ce0c245697c65ed74891dca54cf282860
nemostatus <mattcorralejo1@gmail.com>
2021-05-06 10:19:49 -0700

    testign countdown function it works

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function outer(){
     count = 0
 function countTo(){
-    while(count<100){
+    if(count<100){
     document.getElementById("countDisplay").innerHTML += `${count},`
     count++
-    countTo() //the function is calling itself which checks condition, if it's true then increase count and recal itself until the condition is met
+     countTo() 
     }
 }
-countTo() //the outer function invokes the inner function so onclick the global exe context is the outer function then inner(lifo executed)
-}
-//using a closure is good practice since having variables in global scope can be dangerous
+countTo() }
+
+
 function create(event){
     event.preventDefault()
     count =0
 function inner(){
     
     let num = document.getElementById("num").value
-    while(count < num){
+    if(count < num){
     document.getElementById("divDisplay").innerHTML += `<div> Div number ${count+1}.</div>`
     count++
-    inner()
+     inner() //this was unecessary with the while loop
 }
 }
 inner()}
+
+function javascriptTutorialExample(fromNumber){
+    console.log(fromNumber)
+}
+
+javascriptTutorialExample(10)
